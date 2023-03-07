bf14f6424aacc4e6ba1a6c54ba21dc4f8282dc6b
yoanarobeva <yoana_robeva@yahoo.com>
2023-03-07 20:57:24 +0200

    components added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Header } from './components/Header';
+import { Search } from './components/Search';
+import { Table } from './components/Table';
+import { Pagination } from './components/Pagination';
+import { Footer } from './components/Footer';
+// import { UserDelete } from './components/UserDelete';
+// import { UserCreateEdit } from './components/CreateEdit';
+// import { UserDetails } from './components/UserDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+
+      <main className="main">
+
+        <section className="card users-container">
+
+          <Search />
+
+          {/* <!-- Table component --> */}
+          <Table />
+
+          {/* <!-- New user button  --> */}
+          <button className="btn-add btn">Add new user</button>
+
+          <Pagination />
+
+        </section>
+
+        {/* <!-- User details component  --> */}
+        {/* <UserDetails /> */}
+
+
+        {/* <!-- Create/Edit Form component  --> */}
+        {/* <UserCreateEdit /> */}
+
+
+        {/* <!-- Delete user component  --> */}
+        {/* <UserDelete /> */}
+
+      </main>
+
+      <Footer />
+
+    </>
   );
 }
 
